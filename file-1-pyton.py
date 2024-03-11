fa09832a07c9cc41c4dcf9f9076dab99f8eff396
abbuafzal <162981587+abbuafzal@users.noreply.github.com>
2024-03-11 16:01:33 +0530

    Add files via upload

---
+++ file-1-pyton.py
@@ -0,0 +1,113 @@
+
+'  '  '
+#print("hello world")
+#print("hello")
+
+"""
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+
+print("hello world")
+"""
+
+----> Commenting
+1.) Commenting is used for making documentation in a python file
+2.) When the line is commented, that line will not be considered as a part of the code.
+3.) When the program is executed, commented line will not be in the part of execution.
+> NOTE:
+Single line commenting can be done using # at the begining of the line
+Eg:
+#print("hello 1")
+I Multiple line commenting can be done using ''' code '''. The lines between the quotes will be commented.
+Eg:
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+"""
+print("hello world")
+'  '  '
+#import this
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#Bargained characters
+
+Controls and Data structures → ABC language
+Strings and its formats from C
+Regular expressions → Pearl
+Exception handling Module 3
+Magic methods from → fortron
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+Flavours of Python
+
+*Cpython - Written in C, Most common Implementation of python.
+*Jython Java implementation of Python.
+*IronPython - implementation of the Python programming language
+*which is tightly integrated with .NET.
+*Brython - Browser Python, runs in browsers
+*RubyPython - Bridge between Python and Ruby Interpreters
+*MicroPython Runs in Micro Controllers
+
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+# ---> print(object, sep, end, file, flush)
+#print has 5 parameters
+
+ #object ->the value to be printed is object
+#sep--> seperator
+# Eg
+#a = 8
+#b = 9
+#c = 89
+# print(a, b, c, sep='$')
+
+# end --> which is used to add the string at the end of the value,
+# # It will not keep the command to stay in same line
+# Eg:
+# print(str(a), b, end='%%')
+# print(str(c))
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+#  !  ------> variables
+# ?variable is a name which is used to identify the value of th variable
+# ?which is stored in a specified memory location
+
+
+#  !  rules to define varriables
+# 1.) variable have to start which only alphabets, numbers, underscores
+# 2.) The char following the 1st char can be alphabets, numbers, underscores
+# 3.) No spaces are allowed
+# 4.) No special characters like !@#23 $%^&* ( ) +
+# 5.) case sensitive
+# 6.) Keywords are not allowed as variables
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+# ! ways to define variable
+
+# a = 7
+# b = 9
+
+
+
+
